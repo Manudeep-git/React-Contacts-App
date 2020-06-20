1679ad052ad3e65df3c76fb0f2189b1e995ec9f6
Manudeep-git <38970629+Manudeep-git@users.noreply.github.com>
2020-06-19 19:39:56 -0500

    Search contacts component using forms and controlled components

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,11 +1,57 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function ListContacts(props) { // if we use class ListContacts extends component then should use this.props
+class ListContacts extends React.Component { // if we use class ListContacts extends component then should use this.props
     //console.log('Props', this.props)
+    static propTypes = {
+      contacts: PropTypes.array.isRequired,
+      deleteContact : PropTypes.func.isRequired
+    }
+
+    state ={
+      query : "",
+    }
+    updateQuery = (query) => {
+      this.setState ({
+        query: query.trim()
+      })
+    }
+
+    clearQuery = () => {
+      this.updateQuery('')
+    }
+    // render method
+    render(){
+      const {query}=this.state
+      const {contacts,deleteContact}=this.props
+
+      const showingContacts = query === "" ?
+        contacts
+       :contacts.filter(c=>(
+         c.name.toLowerCase().includes(query.toLowerCase())
+       ))
     return (
+      // Search contacts bar with input field
+      <div className = 'list-contacts'>
+      {/*{JSON.stringify(query)}*/}
+        <div className = "list-contacts-top">
+          <input
+            className="search-contacts"
+            type="text"
+            placeholder = "Search Contacts"
+            value={query}
+            onChange={event => this.updateQuery(event.target.value)}
+          />
+        </div>
+        {/* Display message for search contacts and is valid only when user input matches characters in contacts*/ }
+        {showingContacts.length!== contacts.length && (
+          <div className='showing-contacts'>
+            <span> Now Showing {showingContacts.length} of {contacts.length}</span>
+            <button onClick={this.clearQuery}>Show All</button>
+          </div>
+        )}
       <ol className='contact-list'>
-      {props.contacts.map(contact => (
+      {showingContacts.map(contact => (
         <li key={contact.id} className='contact-list-item'>
           <div
             className='contact-avatar'
@@ -18,19 +64,17 @@ function ListContacts(props) { // if we use class ListContacts extends component
           <p>{contact.handle}</p>
           </div>
           <button
-          onClick ={()=> props.deleteContact(contact)}
+          onClick ={()=> deleteContact(contact)}
           className='contact-remove'>
           remove
           </button>
         </li>
       ))}
       </ol>
+      </div>
     )
+  }
 }
 
-ListContacts.propTypes = {//propTypes is not PropTypes we imported, we are adding it as property
-  contacts: PropTypes.array.isRequired,
-  deleteContact : PropTypes.func.isRequired
-}
 
 export default ListContacts
